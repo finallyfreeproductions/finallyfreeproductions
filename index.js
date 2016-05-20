f33fb65fa71a43715bd3fe425238955773165e1a
jeffreyyourman <jeffreyyourman@gmail.com>
2016-05-20 13:18:25 -0400

    got the carousel working with hover and on click modal

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-$(document).on('click', '#boris1',function(){
-	alert('jeff1');
-});
-$(document).on('click','#boris2', function(){
-	alert('jeff2');
-});
-$(document).on('click','#boris3', function(){
-	alert('jeff3');
-});
-$(document).on('click','#boris4', function(){
-	alert('jeff4');
-});
-$(document).on('click','#boris5', function(){
-	alert('jeff5');
-});
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+// var btn = document.getElementById("boris1");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+// $('#boris1').on('click', function(){
+//    modal.style.display = "block";
+// });
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+$(document).on('click', '#boris1', function(){
+    modal.style.display = "block";
+})
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+$(document).on('mouseenter', '#boris1', function(){$(this).addClass('underline')})
+$(document).on('mouseleave', '#boris1', function(){$(this).removeClass('underline')})
+
+$(document).on('mouseenter', '#boris2', function(){$(this).addClass('underline')})
+$(document).on('mouseleave', '#boris2', function(){$(this).removeClass('underline')})
+
+$(document).on('mouseenter', '#boris3', function(){$(this).addClass('underline')})
+$(document).on('mouseleave', '#boris3', function(){$(this).removeClass('underline')})
+//==============================================================================================
 $(document).on('click', ".carousel-button-right",function(){
 	var carusel = $(this).parents('.carousel');
 	right_carusel(carusel);
